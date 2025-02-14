4c08c937de3a4f27765bce1f0f75938dbbb663e8
und1n3 <undineunda5@gmail.com>
2025-02-14 19:26:51 +0100

    New updates

--- scenes/components/collectible_component.gd
+++ scenes/components/collectible_component.gd
@@ -6,6 +6,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
+		InventoryManager.add_collectable(collectable_name)
 		print("Collected: ", collectable_name)
 		get_parent().queue_free() # parent because will add this to the collectable object scene
 								  # and we want to remove the full scene
---
+++ scripts/globals/inventory_manager.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var inventory: Dictionary = Dictionary()
+
+signal inventory_changed
+
+func add_collectable(collectable_name: String) -> void:
+	inventory.get_or_add(collectable_name) # to add the key if it is not in the dicctionary
+	if inventory[collectable_name] == null: # if it wasnt in the dictionary, the get_or_add added Null
+		inventory[collectable_name] = 1
+	else:
+		inventory[collectable_name] += 1
+		
+	inventory_changed.emit()
